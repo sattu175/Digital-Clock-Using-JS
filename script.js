890042a0a7fdbb9a1218c9eff669e3c6b40656d9
sattu175 <satyammodanwal815@gmail.com>
2026-02-23 16:51:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//ye hame currnet or (live)  time me jo bhi date, time,second,or hiur hai owo hme provide karega
+let a =new Date
+ console.log(a.getHours())
+// console.log(a.getMinutes())
+//console.log(a.getDate())
